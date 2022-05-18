10ff1509aab48b3a5342aed3cefdecbcbc312b04
Braylan Dodson <braylan1005@gmail.com>
2022-05-18 15:39:18 -0400

    commit tree 609992ddcb8641322b3df21688634efd67e6cafd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const names= ["Charlie", "Samip", "Ali"];
+function writeCards(names){
+    const cards=[];
+    for(let i=0;i<names.length;i++){
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+function countDown(startingNum){
+    for(let i =startingNum;i>=0;i--){
+        console.log(i);
+    }
+}
